cbdede82d5df7a1c39cbd3ceb1a5cb8a874f2cd3
Marek Bartík <16442967+marekaf@users.noreply.github.com>
2022-10-13 11:41:30 +0200

    chore: improve heredoc indent (#14)

--- examples/04-aws-wireguard-vpn/outputs.tf
+++ examples/04-aws-wireguard-vpn/outputs.tf
@@ -1,15 +1,15 @@
 output "vpn_config" {
   description = "wireguard VPN client config"
 
-  value = <<END
-# this should come right under your PrivateKey in [Interface] block
-# .2 is the first available address as of now, don't forget to bump this up
-Address = 10.1.0.2/32
+  value = <<-END
+  # this should come right under your PrivateKey in [Interface] block
+  # .2 is the first available address as of now, don't forget to bump this up
+  Address = 10.1.0.2/32
 
-[Peer]
-PublicKey = ${local.wireguard_public_key}
-AllowedIPs = ${var.vpc_cidr}
-Endpoint = ${local.endpoint}:${local.wireguard_port}
+  [Peer]
+  PublicKey = ${local.wireguard_public_key}
+  AllowedIPs = ${var.vpc_cidr}
+  Endpoint = ${local.endpoint}:${local.wireguard_port}
 
 END
 }
