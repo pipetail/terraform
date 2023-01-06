e87594c8d7e071379e51af699246f19a2976544c
Marek Bartik <me@marekbartik.com>
2023-01-06 09:34:27 +0100

    feat: github_oidc arn output

--- modules/github-oidc/outputs.tf
+++ modules/github-oidc/outputs.tf
@@ -7,3 +7,8 @@ output "role_name" {
   value       = aws_iam_role.github_actions.name
   description = "Github Actions IAM Role ARN"
 }
+
+output "oidc_provider_arn" {
+  value       = aws_iam_openid_connect_provider.github.arn
+  description = "OIDC Provider ARN"
+}
