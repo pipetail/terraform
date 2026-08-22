11dc288b462b6c27ae3dfc4ed50e5f577a3173ea
Marek Bartik <marek.bartik@pipetail.io>
2026-08-22 11:00:59 +0200

    fix(kms): scope AWS service key grants

--- modules/kms/variables.tf
+++ modules/kms/variables.tf
@@ -30,6 +30,34 @@ variable "key_user_arns" {
   }
 }
 
+variable "cloudwatch_log_group_arn_patterns" {
+  description = "CloudWatch Logs log group ARN patterns allowed to use the KMS key"
+  type        = set(string)
+  default     = []
+  nullable    = false
+
+  validation {
+    condition = alltrue([
+      for arn in var.cloudwatch_log_group_arn_patterns : can(regex("^arn:(aws|aws-cn|aws-us-gov|aws-iso|aws-iso-b|aws-iso-e|aws-iso-f):logs:[a-z0-9-]+:[0-9]{12}:log-group:[A-Za-z0-9._/#*-]+$", arn))
+    ])
+    error_message = "cloudwatch_log_group_arn_patterns must contain only CloudWatch Logs log group ARN patterns."
+  }
+}
+
+variable "cloudtrail_trail_arns" {
+  description = "CloudTrail trail ARNs allowed to use the KMS key"
+  type        = set(string)
+  default     = []
+  nullable    = false
+
+  validation {
+    condition = alltrue([
+      for arn in var.cloudtrail_trail_arns : can(regex("^arn:(aws|aws-cn|aws-us-gov|aws-iso|aws-iso-b|aws-iso-e|aws-iso-f):cloudtrail:[a-z0-9-]+:[0-9]{12}:trail/[A-Za-z0-9][A-Za-z0-9._-]{1,126}[A-Za-z0-9]$", arn))
+    ])
+    error_message = "cloudtrail_trail_arns must contain only exact CloudTrail trail ARNs."
+  }
+}
+
 variable "region" {
   description = "AWS region name for the KMS key"
   type        = string
